835c2b685fa95b4b069befcb091de193303c1573
PabloMunozP <41455709+PabloMunozP@users.noreply.github.com>
2021-11-09 14:14:41 -0300

    borrar extras

--- src/consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-import json
-consumer = KafkaConsumer('event',
-                         bootstrap_servers=['localhost:9092'],
-                         value_deserializer=lambda m: json.loads(m.decode('ascii')))
-for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
--- src/producer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaProducer
-import json 
-
-producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('ascii'), bootstrap_servers=['localhost:9092'])
-producer.send('event', {'id': 123, 'email_vendedor': 'asdas@mail.com'})
-producer.flush()
